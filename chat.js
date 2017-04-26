5af6224d0dfb3fc748e76ddf2a5ed2d1f6e5d269
Eduardo Pérez <eduardojoseperezf@gmail.com>
2017-04-26 02:09:24 -0300

    (some) emoticons working on chat
    TODO: Escape user inputed raw HTML

--- chat.js
+++ chat.js
@@ -40,9 +40,10 @@ function AppendChat(input, who){
   var person = document.createTextNode(who + " wrote:")
   chat.appendChild(person)
   var text = document.createElement("li");
+  text.innerHTML = ReplaceEmoticons(input)
   text.className = "chat-text";
   chat.appendChild(text);
-  text.innerHTML(ReplaceEmoticons(input));
+
 
   var element = document.getElementById("historybox");
   element.appendChild(chat);
@@ -52,12 +53,43 @@ function AppendChat(input, who){
 
 function ReplaceEmoticons(text) {
   var emoticons = {
-    ':-)' : 'smiley.png',
-    ':)'  : 'smiley.png',
+    ':-)' : 'smile.png',
+    ':)'  : 'smile.png',
     ':D'  : 'grin.png',
     ':-D' : 'grin.png',
-    ';)'  : 'wink.png',
-    ';-)' : 'wink.png'
+    ':d'  : 'grin.png',
+    ':-d' : 'grin.png',
+    ';)'  : 'wink.gif',
+    ';-)' : 'wink.gif',
+    ":'(" : 'crying.gif',
+    ':O'  : 'surprised.png',
+    ':o'  : 'surprised.png',
+    ':-O' : 'surprised.png',
+    ':-o' : 'surprised.png',
+    ':p'  : 'tonguesmile.png',
+    ':P'  : 'tonguesmile.png',
+    ':-p' : 'tonguesmile.png',
+    ':-P' : 'tonguesmile.png',
+    '(h)' : 'cool.png',
+    '(H)' : 'cool.png',
+    ':@'  : 'angry.png',
+    ':$'  : 'shy.png',
+    '(SO)' : 'soccer.png',
+    '(so)' : 'soccer.png',
+    ':s'  : 'confused.png',
+    ':S'  : 'confused.png',
+    ':('  : 'sad.png',
+    ':-(' : 'sad.png',
+    ':|'  : 'disappointed.png',
+    ':-|' : 'disappointed.png',
+    '(6)' : 'devil.png',
+    '(a)' : 'angel.png',
+    '(A)' : 'angel.png',
+    '(l)' : 'heart.png',
+    '(L)' : 'heart.png',
+    '(m)' : 'msnmsgr.png',
+    '(M)' : 'msnmsgr.png'
+
   }, url = "img/msn75/emoticons/", patterns = [],
      metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
@@ -67,7 +99,6 @@ function ReplaceEmoticons(text) {
       patterns.push('('+i.replace(metachars, "\\$&")+')');
     }
   }
-
   // build the regular expression and replace
   return text.replace(new RegExp(patterns.join('|'),'g'), function (match) {
     return typeof emoticons[match] != 'undefined' ?
