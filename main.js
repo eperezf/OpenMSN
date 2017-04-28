d9f22fadc203599835c4720394b9102cff7f8517
Eduardo Pérez <eduardojoseperezf@gmail.com>
2017-04-28 17:10:29 -0300

    Missed some linting on main.js

--- main.js
+++ main.js
@@ -64,7 +64,7 @@ ipcRenderer.on('status-change', (event, arg) => {
 function SetStatus (selectObject) {
   var newi_status = selectObject.value;
   if (newi_status.includes("cfg")){
-    openConfig()
+    openConfig();
     console.log("CONFIG SELECTION");
     nickstat = nickname + " ("+ status + ")";
     document.getElementById('statusselect').value="show";
@@ -75,7 +75,7 @@ function SetStatus (selectObject) {
       nickstat = nickname + " ("+ status + ")";
       document.getElementById('statusselect').value="show";
       document.getElementById('nickstat').textContent = nickstat;
-      ipcRenderer.send('status-change', newi_status)
+      ipcRenderer.send('status-change', newi_status);
     }
   }
 }
@@ -86,99 +86,99 @@ function SetStatus (selectObject) {
 
 ipcRenderer.on('contact-i_status-change', (event, arg) => {
   from = arg.from.split("/")[0];
-  console.log('Contact ' + arg.from + ' changed his internal status to ' + arg.i_status)
-  ci_statusChange(from, arg.i_status)
-})
+  console.log('Contact ' + arg.from + ' changed his internal status to ' + arg.i_status);
+  ci_statusChange(from, arg.i_status);
+});
 
 ipcRenderer.on('contact-status-change', (event, arg) => {
   from = arg.from.split("/")[0];
-  console.log('Contact ' + arg.from + ' changed his status to ' + arg.status)
-  cstatusChange(from, arg.status)
-})
+  console.log('Contact ' + arg.from + ' changed his status to ' + arg.status);
+  cstatusChange(from, arg.status);
+});
 
 ipcRenderer.on('contact-nickname-change', (event, arg) => {
-  document.getElementById(arg.email).getElementsByClassName("cnickname")[0].textContent = arg.nickname
-  console.log('Contact ' + arg.from + ' changed his status to ' + arg.nickname)
-})
+  document.getElementById(arg.email).getElementsByClassName("cnickname")[0].textContent = arg.nickname;
+  console.log('Contact ' + arg.from + ' changed his status to ' + arg.nickname);
+});
 
 ipcRenderer.on('insert-contact', (event, arg) =>{
-  console.log("Contact info received: " + arg.email + " is " + arg.status + " (" + arg.i_status + ")")
-  if (document.getElementById(arg.email) == null) {
-    if (arg.nickname == undefined){
+  console.log("Contact info received: " + arg.email + " is " + arg.status + " (" + arg.i_status + ")");
+  if (document.getElementById(arg.email) === null) {
+    if (arg.nickname === undefined){
       AppendContact(arg.email, arg.email, arg.status, arg.i_status);
     }
     else {
       AppendContact(arg.nickname, arg.email, arg.status, arg.i_status);
     }
   }
-})
+});
 
 
 function openConfig(defaulttab) {
-  ipcRenderer.send('open-config', defaulttab)
-  console.log("Opening config!")
+  ipcRenderer.send('open-config', defaulttab);
+  console.log("Opening config!");
 }
 
 function addContact(){
-  ipcRenderer.send('open-add')
-  console.log("Opening Add Contact form!")
+  ipcRenderer.send('open-add');
+  console.log("Opening Add Contact form!");
 }
 
 function AppendContact(nickname, jid, status, i_status){
   console.log("appending " + jid + " i_status " + i_status);
-  var onlinelist = document.getElementById('onlinelist')
-  var offlinelist = document.getElementById('offlinelist')
+  var onlinelist = document.getElementById('onlinelist');
+  var offlinelist = document.getElementById('offlinelist');
 
-  var contactitem = document.createElement("div")
+  var contactitem = document.createElement("div");
   contactitem.setAttribute("id", jid);
-  contactitem.setAttribute("ondblclick", "OpenChat('" + jid + "')")
+  contactitem.setAttribute("ondblclick", "OpenChat('" + jid + "')");
   contactitem.classList.add("contactitem");
-  contactitem.setAttribute("onclick", "clickSingleA(this)")
+  contactitem.setAttribute("onclick", "clickSingleA(this)");
 
-  var statimage = document.createElement("img")
-  statimage.src = "img/msn75/"+ i_status +".png"
+  var statimage = document.createElement("img");
+  statimage.src = "img/msn75/"+ i_status +".png";
   statimage.classList.add("statimage");
 
 
-  var cnicknameitem = document.createElement("div")
+  var cnicknameitem = document.createElement("div");
   cnicknameitem.className += "cnickname inline";
   cnicknameitem.appendChild(document.createTextNode(nickname));
 
-  var cstatusitem = document.createElement("div")
+  var cstatusitem = document.createElement("div");
   cstatusitem.className += "cstatus inline";
   cstatusitem.appendChild(document.createTextNode("(" + status + ")"));
 
-  var cpmessageitem = document.createElement("div")
+  var cpmessageitem = document.createElement("div");
   cpmessageitem.className += "cpmessage inline";
   cpmessageitem.appendChild(document.createTextNode(" - Not Available"));
 
-  contactitem.appendChild(statimage)
-  contactitem.appendChild(cnicknameitem)
-  contactitem.appendChild(cstatusitem)
-  contactitem.appendChild(cpmessageitem)
+  contactitem.appendChild(statimage);
+  contactitem.appendChild(cnicknameitem);
+  contactitem.appendChild(cstatusitem);
+  contactitem.appendChild(cpmessageitem);
 
   if (i_status == "unavailable"){
-    offlinelist.appendChild(contactitem)
+    offlinelist.appendChild(contactitem);
   }
   else {
-    onlinelist.appendChild(contactitem)
+    onlinelist.appendChild(contactitem);
   }
 
 
 }
 
 function cstatusChange(from, status){
-  var cline = document.getElementById(from)
-  var cnick = cline.getElementsByClassName('cstatus')[0]
-  cnick.textContent = '(' + status + ')'
+  var cline = document.getElementById(from);
+  var cnick = cline.getElementsByClassName('cstatus')[0];
+  cnick.textContent = '(' + status + ')';
 
 }
 
 function ci_statusChange(from, i_status){
-  var cline = document.getElementById(from)
-  var cicon = cline.getElementsByClassName('statimage')[0]
+  var cline = document.getElementById(from);
+  var cicon = cline.getElementsByClassName('statimage')[0];
 
-  var cstatus = cline.getElementsByClassName('cstatus')[0]
+  var cstatus = cline.getElementsByClassName('cstatus')[0];
   if (cstatus.textContent == "(Offline)"){
     if (i_status == "available"){
       document.getElementById('onlinelist').appendChild(document.getElementById(from));
@@ -190,11 +190,11 @@ function ci_statusChange(from, i_status){
       document.getElementById('offlinelist').appendChild(document.getElementById(from));
     }
   }
-  cicon.src = "img/msn75/" + i_status + ".png"
+  cicon.src = "img/msn75/" + i_status + ".png";
 }
 
 function OpenChat(email){
-  ipcRenderer.send('open-chat', email)
+  ipcRenderer.send('open-chat', email);
 }
 
 function clickSingleA(a)
